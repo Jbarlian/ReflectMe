8a1da080b476ff3d636308a853f668d9b809baf0
Fauzan Ramadhan <35336229+ojinsyo@users.noreply.github.com>
2020-04-07 11:16:42 +0700

    detail page

---
+++ ReflectMe/detailpageVC.swift
@@ -0,0 +1,38 @@
+//
+//  detailpageVC.swift
+//  ReflectMe
+//
+//  Created by Fauzan Ramadhan on 07/04/20.
+//  Copyright © 2020 Group 11 - Apple Developer Academy. All rights reserved.
+//
+
+import UIKit
+
+class detailpageVC: UIViewController {
+
+    @IBOutlet weak var reflectionEmotionBg: UIImageView!
+    @IBOutlet weak var reflectionDate: UILabel!
+    @IBOutlet weak var reflectionMonthYear: UILabel!
+    @IBOutlet weak var reflectionDay: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
