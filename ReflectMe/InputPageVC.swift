76750bd9441db4c40024726fdf84962816ff7498
James Barlian <Jamesbarlian@gmail.com>
2020-04-07 12:39:35 +0700

    input page semi-done

--- ReflectMe/InputPageVC.swift
+++ ReflectMe/InputPageVC.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class InputPageVC: UIViewController {
+class InputPageVC: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var doText: UITextView!
+    @IBOutlet weak var thoughtText: UITextView!
     
+    var placeHolderLabel: UILabel!
     
     func GetCurrentDate(){
         // Gets tne current date
@@ -24,12 +27,58 @@ class InputPageVC: UIViewController {
         dateLabel.text = format.string(from: currentDateTime)
     }
     
+    func FeelingsPlaceHolder () {
+        doText.delegate = self
+        placeHolderLabel = UILabel()
+        placeHolderLabel.text = "Tell us about your day :)"
+        placeHolderLabel.font = UIFont.italicSystemFont(ofSize: (doText.font?.pointSize)!)
+        placeHolderLabel.sizeToFit()
+        doText.addSubview(placeHolderLabel)
+        placeHolderLabel.frame.origin = CGPoint(x: 5, y: (doText.font?.pointSize)! / 2)
+        placeHolderLabel.textColor = UIColor.lightGray
+        placeHolderLabel.isHidden = !doText.text.isEmpty
+    }
    
+    func ThoughtsPlaceHolder (){
+        thoughtText.delegate = self
+        placeHolderLabel = UILabel()
+        placeHolderLabel.text = "Tell us what's on your mind for today :)"
+        placeHolderLabel.font = UIFont.italicSystemFont(ofSize: (thoughtText.font?.pointSize)!)
+        placeHolderLabel.sizeToFit()
+        thoughtText.addSubview(placeHolderLabel)
+        placeHolderLabel.frame.origin = CGPoint(x: 5, y: (thoughtText
+            .font?.pointSize)! / 2)
+        placeHolderLabel.textColor = UIColor.lightGray
+        placeHolderLabel.isHidden = !thoughtText.text.isEmpty
+    }
+    
+    
+    func doTextDidChange(_ doText: UITextView) {
+        placeHolderLabel.isHidden = !doText.text.isEmpty
+    }
+    
+    func thoughtTextDidChange(_ thoughtText: UITextView) {
+        placeHolderLabel.isHidden = !thoughtText.text.isEmpty
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        
+        // Insert placeholders
+        FeelingsPlaceHolder()
+        ThoughtsPlaceHolder()
+        
+        //Get rid of placeholder
+        doTextDidChange(doText)
+        thoughtTextDidChange(thoughtText)
+        
+        // Print Today's Date
         GetCurrentDate()
+        
+        
+        
     }
 
 
